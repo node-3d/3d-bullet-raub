822483dfc8ce550c9404402a52d7674287eaca67
ipakhotin <xxotkpobehuexx@gmail.com>
2018-01-18 09:00:40 +0600

    add Cylinder, Sphere

---
+++ firgures/cylinder.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { three } = require('node-3d-ready-raub');
+const { Body } = require('node-bullet-raub');
+
+class Cylinder {
+	constructor(opts) {
+		const { screen, scene } = opts;
+
+		const pos = opts.pos || { x: 0, y: 0, z: 0 };
+		const size = opts.size || { x: 1, y: 1, z: 1 };
+		const mass = opts.mass || 1;
+
+		const geometry = new three.CylinderGeometry(size.x, size.x, size.y, size.z);
+		const material = new three.MeshLambertMaterial({
+			color: Math.round(0xffffff * Math.random())
+			// map: new THREE.TextureLoader().load('TODO'),
+		});
+		const mesh = new three.Mesh(geometry, material);
+		screen.scene.add(mesh);
+
+		mesh.position.set(pos.x, pos.y, pos.z);
+
+		const body = new Body({ scene: opts.scene });
+
+		body.type = 'roll';
+		body.pos = pos;
+		body.size = { x: 6, y: 20, z: 32 };
+		body.mass = mass;
+
+		body.on('update', ({ pos, quat }) => {
+			mesh.position.set(pos.x, pos.y, pos.z);
+			mesh.quaternion.set(quat.x, quat.y, quat.z, quat.w);
+		});
+	}
+}
+
+module.exports = Cylinder;
---
+++ index4.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const { Screen, three, loop } = require('node-3d-ready-raub');
+const { Scene, Body } = require('node-bullet-raub');
+
+const Cone = require('./firgures/cone');
+const Cylinder = require('./firgures/cylinder');
+
+//-----------------------------------------------------------------------------------
+// Don't work
+//var spriteMap = new THREE.TextureLoader().load(__dirname + '/textures/sprite.png');
+//var spriteMap = new THREE.TextureLoader().load(__dirname + '/textures/sprite2.png');
+//var spriteMap = new THREE.TextureLoader().load(__dirname + '/textures/texture3.gif');
+
+var spriteMap = new THREE.TextureLoader().load(__dirname + '/textures/crate.gif');
+//-----------------------------------------------------------------------------------
+
+const screen = new Screen();
+screen.camera.position.z = 60;
+screen.camera.position.y = 20;
+screen.camera.up = new three.Vector3(0, 1, 0);
+screen.camera.lookAt(new three.Vector3(0, 0, 0));
+
+loop(screen);
+
+const pgeo = new THREE.PlaneGeometry(200, 200, 4, 4);
+
+//-----------------------------------------------------------------------------------
+// Don't work
+//const pmat = new THREE.SpriteMaterial({ map: spriteMap, color: 0xffffff });
+
+var spriteMaterial = new THREE.MeshBasicMaterial({ map: spriteMap });
+//-----------------------------------------------------------------------------------
+
+const pmesh = new THREE.Mesh(pgeo, spriteMaterial);
+
+screen.scene.add(pmesh);
+pmesh.rotation.x = -Math.PI * 0.5;
+//-----
+
+const light = new three.AmbientLight(0x666666); // soft white light
+screen.scene.add(light);
+
+const pointLight = new three.PointLight(0xffffff, 1, 100000);
+screen.scene.add(pointLight);
+pointLight.position.x = 200;
+pointLight.position.y = 2000;
+pointLight.position.z = 500;
+
+const scene = new Scene();
+const _draw = screen.draw.bind(screen);
+screen.draw = () => {
+	scene.update();
+	_draw();
+};
+
+const plane = new Body({ scene });
+plane.type = 'plane';
+
+//console.log('plane', plane.getAabb());
+
+for (let i = 0; i < 20; i++) {
+	//const size1 = 4 * Math.random();
+	//const size2 = 64 * Math.random();
+	new Cylinder({
+		screen,
+		scene,
+		pos: {
+			x: 20 * Math.random(),
+			y: 20 + 5 * Math.random(),
+			z: 5 * Math.random()
+		},
+		size: {
+			x: 3,
+			y: 20,
+			z: 30
+		},
+		mass: 1 + 2 * Math.random()
+	});
+}
