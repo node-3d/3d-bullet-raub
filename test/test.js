84dee299772818fa6b01baf88db93fd763179e83
Luis Blanco <blanco@ucoz-team.net>
2018-02-05 09:58:35 +0300

    update dep, add test

---
+++ test/test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const { expect } = require('chai');
+
+const plugin = require('3d-bullet-raub');
+
+
+describe('Node.js 3D Bullet', () => {
+	
+	it(`exports a function`, () => {
+		expect(plugin).to.be.a('function');
+	});
+	
+	// TODO: tests, mocks
+	
+});
