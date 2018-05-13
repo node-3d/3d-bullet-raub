ec02014549423ab2094714cf2b30e5154b1a6e2f
Luis Blanco <raubtierxxx@gmail.com>
2018-05-13 23:00:48 +0300

    WIP: Update

--- examples/index.js
+++ examples/index.js
@@ -7,92 +7,92 @@ const { getRandom, getRandomPosition } = require('./utils/random');
 const { screen, scene } = require('./utils/world');
 
 
-bullet3d(core3d);
+// bullet3d(core3d);
 
 
-const { three, bullet, Image, doc } = core3d;
-const { Box, Sphere, Cylinder, Capsule } = bullet;
+// const { three, bullet, Image, doc } = core3d;
+// const { Box, Sphere, Cylinder, Capsule } = bullet;
 
 
-const icon = new Image();
-icon.src = __dirname + '/bullet.ico';
-icon.on('load', () => doc.icon = icon);
+// const icon = new Image();
+// icon.src = __dirname + '/bullet.ico';
+// icon.on('load', () => doc.icon = icon);
 
-doc.title = 'All Shapes';
-
-
-const raycaster = new three.Raycaster();
-const mouse = new three.Vector2();
+// doc.title = 'All Shapes';
 
 
-screen.on('mousedown', e => {
+// const raycaster = new three.Raycaster();
+// const mouse = new three.Vector2();
+
+
+// screen.on('mousedown', e => {
 	
-	mouse.x = ( e.x / screen.w ) * 2 - 1;
-	mouse.y = - ( e.y / screen.h ) * 2 + 1;
+// 	mouse.x = ( e.x / screen.w ) * 2 - 1;
+// 	mouse.y = - ( e.y / screen.h ) * 2 + 1;
 	
-	raycaster.setFromCamera( mouse, screen.camera );
-	const ray = raycaster.ray;
+// 	raycaster.setFromCamera( mouse, screen.camera );
+// 	const ray = raycaster.ray;
 	
-	const start = ray.origin;
-	const end = ray.at(100000);
+// 	const start = ray.origin;
+// 	const end = ray.at(100000);
 	
-	const { body } = scene.hit(start, end);
+// 	const { body } = scene.hit(start, end);
 	
-	if (body && body.mass) {
-		if (e.button === 0) {
-			body.vell = [0, 100 * Math.random(), 0];
-		} else if (e.button === 1) {
-			body.destroy();
-		}
+// 	if (body && body.mass) {
+// 		if (e.button === 0) {
+// 			body.vell = [0, 100 * Math.random(), 0];
+// 		} else if (e.button === 1) {
+// 			body.destroy();
+// 		}
 		
-	}
+// 	}
 	
-});
+// });
 
 
-const getCommonOpts = () => ({
-	screen,
-	scene,
-	pos: getRandomPosition(),
-	mass: getRandom(0.3, 4),
-	color: Math.floor(0xFFFFFF) * Math.random(),
-});
+// const getCommonOpts = () => ({
+// 	screen,
+// 	scene,
+// 	pos: getRandomPosition(),
+// 	mass: getRandom(0.3, 4),
+// 	color: Math.floor(0xFFFFFF) * Math.random(),
+// });
 
 
-const createCylinder = () => new Cylinder({
-	...getCommonOpts(),
-	radius: getRandom(0.5, 2),
-	height: getRandom(5, 10),
-});
+// const createCylinder = () => new Cylinder({
+// 	...getCommonOpts(),
+// 	radius: getRandom(0.5, 2),
+// 	height: getRandom(5, 10),
+// });
 
 
-const createBox = () => new Box({
-	...getCommonOpts(),
-	size: { x: getRandom(1, 5), y: getRandom(1, 5), z: getRandom(1, 5) },
-});
+// const createBox = () => new Box({
+// 	...getCommonOpts(),
+// 	size: { x: getRandom(1, 5), y: getRandom(1, 5), z: getRandom(1, 5) },
+// });
 
 
-const createSphere = () => new Sphere({
-	...getCommonOpts(),
-	radius: getRandom(2, 3),
-});
+// const createSphere = () => new Sphere({
+// 	...getCommonOpts(),
+// 	radius: getRandom(2, 3),
+// });
 
 
-const createCapsule = () => new Capsule({
-	...getCommonOpts(),
-	radius: getRandom(0.5, 2),
-	height: getRandom(5, 10),
-});
+// const createCapsule = () => new Capsule({
+// 	...getCommonOpts(),
+// 	radius: getRandom(0.5, 2),
+// 	height: getRandom(5, 10),
+// });
 
 
-for (let i = 0; i < 40; i++) {
+// for (let i = 0; i < 40; i++) {
 	
-	createBox();
+// 	createBox();
 	
-	createCylinder();
+// 	createCylinder();
 	
-	createCapsule();
+// 	createCapsule();
 	
-	createSphere();
+// 	createSphere();
 	
-}
+// }
--- examples/utils/world.js
+++ examples/utils/world.js
@@ -16,7 +16,7 @@ const loadTexture = fileName => new THREE.TextureLoader().load(`textures/${fileN
 
 const spriteMap = loadTexture('texture_1.jpg');
 
-//-----------------------------------------------------------------------------------
+// //-----------------------------------------------------------------------------------
 
 const screen = new Screen();
 screen.camera.position.z = 60;
@@ -94,31 +94,31 @@ var spriteMaterial = new THREE.MeshBasicMaterial({ map: spriteMap });
 
 const pmesh = new THREE.Mesh(pgeo, spriteMaterial);
 screen.scene.add(pmesh);
-pmesh.rotation.x = -Math.PI * 0.5;
+// pmesh.rotation.x = -Math.PI * 0.5;
 //-----
 
 
-const light = new three.AmbientLight(0x666666); // soft white light
-screen.scene.add(light);
+// const light = new three.AmbientLight(0x666666); // soft white light
+// screen.scene.add(light);
 
 
-const pointLight = new three.PointLight(0xffffff, 1, 100000);
-screen.scene.add(pointLight);
-pointLight.position.x = 200;
-pointLight.position.y = 2000;
-pointLight.position.z = 500;
+// const pointLight = new three.PointLight(0xffffff, 1, 100000);
+// screen.scene.add(pointLight);
+// pointLight.position.x = 200;
+// pointLight.position.y = 2000;
+// pointLight.position.z = 500;
 
 
-const scene = new Scene();
-const _draw = screen.draw.bind(screen);
-screen.draw = () => {
-	scene.update();
-	_draw();
-};
+// const scene = new Scene();
+// const _draw = screen.draw.bind(screen);
+// screen.draw = () => {
+// 	scene.update();
+// 	_draw();
+// };
 
 
-const plane = new Body({ scene });
-plane.type = 'plane';
+// const plane = new Body({ scene });
+// plane.type = 'plane';
 
 
-module.exports = { screen, scene };
+// module.exports = { screen, scene };
