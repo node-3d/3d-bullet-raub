fbddd6d4277118e8686a63c3564d340fd9ca1fa1
Luis Blanco <luisblanco1337@gmail.com>
2020-01-14 19:44:28 +0300

    Update dependency

--- examples/index.js
+++ examples/index.js
@@ -8,7 +8,7 @@ const { three, bullet, Image, doc, Screen, loop } = init({ plugins: [bullet3d] }
 const { Box, Ball, Roll, Caps, Scene, Body } = bullet;
 
 const icon = new Image();
-icon.src = __dirname + '/bullet.ico';
+icon.src = __dirname + '/bullet.png';
 icon.on('load', () => doc.icon = icon);
 
 doc.title = 'All Shapes';
